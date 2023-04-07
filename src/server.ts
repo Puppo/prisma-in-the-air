c5a302156a891238d149369130229945099d72ba
Luca Del Puppo <luca@delpuppo.net>
2023-04-07 13:05:43 +0200

    move to fastify 4 and node 18

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import fastify from "fastify";
 
 const PORT = Number.parseInt(process.env.PORT || "3000");
 
 const server = fastify({
   logger: true,
-});
+}).withTypeProvider<TypeBoxTypeProvider>();
 
 server.register(import("./app"));
 
